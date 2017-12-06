2cd778ffd8c7feaa04a51ee4722c3abe72c18353
Brajesh Singh <brajesh@cosmiccoders.com>
2017-12-06 13:23:33 +0530

    indentation, docblock, spelling fixes

--- username-availability-checker.php
+++ username-availability-checker.php
@@ -15,107 +15,140 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class BuddyDev_Username_Availability_Checker {
 
+	/**
+	 * Singleton instance.
+	 *
+	 * @var BuddyDev_Username_Availability_Checker
+	 */
 	private static $instance = null;
+	/**
+	 * Absolute path to this plugin directory.
+	 *
+	 * @var string
+	 */
 	private $path;
+
+	/**
+	 * Absolute url to this plugin directory.
+	 *
+	 * @var string
+	 */
 	private $url;
 
+	/**
+	 * Constructor.
+	 */
 	private function __construct() {
 
 		$this->path = plugin_dir_path( __FILE__ );
-		$this->url = plugin_dir_url( __FILE__ );
+		$this->url  = plugin_dir_url( __FILE__ );
 
-		$this->setup_hooks();
+		$this->setup();
 	}
 
+	/**
+	 * Get the singleton instance.
+	 *
+	 * @return BuddyDev_Username_Availability_Checker
+	 */
 	public static function get_instance() {
 
-		if( is_null(self::$instance ) ) {
+		if ( is_null( self::$instance ) ) {
 			self::$instance = new self();
 		}
 
 		return self::$instance;
 	}
 
-
-	private function setup_hooks() {
-		//load translations
+	/**
+	 * Setup hooks.
+	 */
+	private function setup() {
+		// load translations.
 		add_action( 'init', array( $this, 'load_textdomain' ) );
 
-		//load css/js on front end
+		// load css/js on front end.
 		add_action( 'wp_head', array( $this, 'add_ajax_url' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_js' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_css' ) );
-		//on wp-login.php for action=register
+		// on wp-login.php for action=register.
 		add_action( 'login_enqueue_scripts', array( $this, 'load_js' ) );
 		add_action( 'login_enqueue_scripts', array( $this, 'load_css' ) );
 		add_action( 'login_head', array( $this, 'add_ajax_url' ) );
 
-		//load assets on admin Add new user screen
+		// load assets on admin Add new user screen.
 		add_action( 'admin_enqueue_scripts', array( $this, 'load_js' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'load_css' ) );
-		//ajax check
-		add_action( 'wp_ajax_check_username', array( $this, 'ajax_check_username' ) );//hook to ajax action
-		add_action( 'wp_ajax_nopriv_check_username', array( $this, 'ajax_check_username' ) );//hook to ajax action
+		// ajax check.
+		// hook to ajax action.
+		add_action( 'wp_ajax_check_username', array( $this, 'ajax_check_username' ) );
+		// hook to ajax action.
+		add_action( 'wp_ajax_nopriv_check_username', array( $this, 'ajax_check_username' ) );
 
 	}
 
-    public function load_textdomain() {
-
+	/**
+	 * Load translations.
+	 */
+	public function load_textdomain() {
 		load_plugin_textdomain( 'bpdev-username-availability-checker', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-    }
+	}
 
+	/**
+	 * Check username via ajax.
+	 */
 	public function ajax_check_username() {
 
-		if( empty( $_POST['user_name'] ) ) {
+		if ( empty( $_POST['user_name'] ) ) {
 
+			// if username is empty, the execution will stop here.
 			wp_send_json( array(
-				'code'		=> 'error',
-				'message'	=> __( 'Username Can not be empty!', 'bpdev-username-availability-checker' )
-			));
-			//if uusername is empty, the execution wills top here
+				'code'    => 'error',
+				'message' => __( 'Username Can not be empty!', 'bpdev-username-availability-checker' ),
+			) );
 		}
 
 
-		$user_name = sanitize_user( $_POST['user_name'] ) ;
+		$user_name = sanitize_user( $_POST['user_name'] );
 
-		if( username_exists( $user_name ) ) {
+		if ( username_exists( $user_name ) ) {
 
 			$message = array(
-				'code'		=> 'taken',
-				'message'	=> __( 'This usename is taken, please choose another one.','bpdev-username-availability-checker' )
+				'code'    => 'taken',
+				'message' => __( 'This usename is taken, please choose another one.', 'bpdev-username-availability-checker' ),
 			);
 
 		} elseif ( is_multisite() ) {
-			//for mu
+			// for multisite.
 			global $wpdb;
-			 //check for the username in the signups table
+			// check for the username in the signups table.
 			$user = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->signups WHERE user_login = %s", $user_name ) );
 
-			if( ! empty( $user ) ) {
+			if ( ! empty( $user ) ) {
 				$message = array(
-					'code'		=> 'registered',
-					'message'	=>__( 'This username is registered but not activated. It may be available within few days if not activated. Please check back again for the availability.', 'bpdev-username-availability-checker' )
+					'code'    => 'registered',
+					'message' => __( 'This username is registered but not activated. It may be available within few days if not activated. Please check back again for the availability.', 'bpdev-username-availability-checker' ),
 				);
 			}
 		}
 
-		if( empty( $message ) ) {//so all is well, but now let us validate
+		if ( empty( $message ) ) {
+			// so all is well, but now let us validate.
 			$check = $this->validate_username( $user_name );
 
 			if ( empty( $check ) ) {
 				$message = array(
-					'code'		=> 'success',
-					'message'	=> __( 'Congrats! The username is available.', 'bpdev-username-availability-checker' )
+					'code'    => 'success',
+					'message' => __( 'Congrats! The username is available.', 'bpdev-username-availability-checker' ),
 				);
 			} else {
 
 				$message = array(
-					'code'		=> 'error',
-					'message'	=> $check
+					'code'    => 'error',
+					'message' => $check,
 				);
 			}
 		}
-
 
 
 		wp_send_json( $message );
@@ -126,62 +159,70 @@ class BuddyDev_Username_Availability_Checker {
 	 */
 	public function load_js() {
 
-		if( $this->should_load_asset() ) {
-
-			wp_enqueue_script( 'username-availability-checker-js', $this->url . "assets/username-availability-checker.js", array( 'jquery' ) );
+		if ( $this->should_load_asset() ) {
+			wp_enqueue_script( 'username-availability-checker-js', $this->url . 'assets/username-availability-checker.js', array( 'jquery' ) );
 
 			$data = array(
-				'selectors' => apply_filters( 'buddydev_uachecker_selectors', 'input#signup_username, form#createuser input#user_login, #registerform input#user_login, .lwa-register input#user_login' )
+				'selectors' => apply_filters( 'buddydev_uachecker_selectors', 'input#signup_username, form#createuser input#user_login, #registerform input#user_login, .lwa-register input#user_login' ),
 			);
 
 			wp_localize_script( 'username-availability-checker-js', '_BDUAChecker', $data );
 		}
 	}
 
+	/**
+	 * Load css.
+	 */
 	public function load_css() {
 
-		if( $this->should_load_asset() ) {
+		if ( $this->should_load_asset() ) {
 			wp_enqueue_style( 'username-availability-checker-css', $this->url . 'assets/username-availability-checker.css' );
 		}
 	}
 
+	/**
+	 * Add ajax end point.
+	 */
 	public function add_ajax_url() {
-	?>
-	<script type="text/javascript">
-		var ajaxurl = "<?php echo admin_url('admin-ajax.php');?>";
-	</script>
-	<?php
+		?>
+        <script type="text/javascript">
+            var ajaxurl = "<?php echo admin_url( 'admin-ajax.php' );?>";
+        </script>
+		<?php
 	}
+
 	/**
 	 * Check whether to load assets or not?
 	 *
-	 * @return boolean whether t load assets or not
+	 * @return boolean whether to load assets or not
 	 */
 	public function should_load_asset() {
 		global $pagenow;
 
 		$load = false;
 
-		if( function_exists( 'bp_is_register_page' ) && bp_is_register_page() ) {
+		if ( function_exists( 'bp_is_register_page' ) && bp_is_register_page() ) {
 			$load = true;
-		} elseif ( is_admin() && get_current_screen()->id == 'user' && get_current_screen()->action =='add' ) {
+		} elseif ( is_admin() && get_current_screen()->id == 'user' && get_current_screen()->action == 'add' ) {
 			$load = true;
-		} elseif( $pagenow == 'wp-login.php' && isset( $_GET['action'] ) && $_GET['action'] =='register' ) {
+		} elseif ( $pagenow == 'wp-login.php' && isset( $_GET['action'] ) && $_GET['action'] == 'register' ) {
 			$load = true;
-		} elseif( class_exists( 'LoginWithAjax' ) && ! is_user_logged_in() ) {
+		} elseif ( class_exists( 'LoginWithAjax' ) && ! is_user_logged_in() ) {
 			$load = true;
 		}
-		//sorry I should have renamed it buddydev_uachecker__load_assets but now I can not, my hads are tied
-		return apply_filters( 'buddydev_username_availability_checker_load_assets', $load  );
+
+		// sorry I should have renamed it buddydev_uachecker__load_assets but now I can not, my hads are tied.
+		return apply_filters( 'buddydev_username_availability_checker_load_assets', $load );
 
 	}
 
 
-	/* Helper function to check the username is valid or not,
-	 * thanks to @apeatling, taken from bp-core/bp-core-signup.php and modified for chacking only the username
+	/**
+	 * Helper function to check the username is valid or not,
+	 * Thanks to @apeatling, taken from bp-core/bp-core-signup.php and modified for checking only the username
 	 * original: bp_core_validate_user_signup()
 	 *
-	 * @return string fnothing if validated else error string
+	 * @return string nothing if validated else error string
 	 * */
 	private function validate_username( $user_name ) {
 
@@ -198,15 +239,15 @@ class BuddyDev_Username_Availability_Checker {
 		//update_site_option( 'illegal_names', $illegal_names );
 
 		if ( ! validate_username( $user_name ) || in_array( $user_name, ( array ) $illegal_names ) || ( isset( $maybe[0] ) && $user_name != $maybe[0] ) ) {
-		   $error= __( 'Only lowercase letters and numbers allowed', 'bpdev-username-availability-checker' );
+			$error = __( 'Only lowercase letters and numbers allowed', 'bpdev-username-availability-checker' );
 		}
 
 		if ( strlen( $user_name ) < 4 ) {
-		   $error=  __( 'Username must be at least 4 characters', 'buddypress' ) ;
+			$error = __( 'Username must be at least 4 characters', 'buddypress' );
 		}
 
 		if ( strpos( ' ' . $user_name, '_' ) != false ) {
-			$error= __( 'Sorry, usernames may not contain the character "_"!', 'bpdev-username-availability-checker' ) ;
+			$error = __( 'Sorry, usernames may not contain the character "_"!', 'bpdev-username-availability-checker' );
 		}
 
 		/* Is the user_name all numeric? */
@@ -215,12 +256,12 @@ class BuddyDev_Username_Availability_Checker {
 		preg_match( '/[0-9]*/', $user_name, $match );
 
 		if ( $match[0] == $user_name ) {
-			$error= __( 'Sorry, usernames must have letters too!', 'bpdev-username-availability-checker' ) ;
+			$error = __( 'Sorry, usernames must have letters too!', 'bpdev-username-availability-checker' );
 		}
 
-		//Let others dictate us
-		//the devine message to show the users in case of failure
-		//success is empty, never forget that.
+		// Let others dictate us
+		// the divine message to show the users in case of failure
+		// success is empty, never forget that.
 		return apply_filters( 'buddydev_uachecker_username_error', $error, $user_name );
 
 	}
@@ -228,5 +269,5 @@ class BuddyDev_Username_Availability_Checker {
 
 }
 
-//instantiate
+// instantiate.
 BuddyDev_Username_Availability_Checker::get_instance();
